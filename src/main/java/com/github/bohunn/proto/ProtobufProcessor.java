f96b4e0374447ce2a5783c1c4ef8e874365b1362
bohunn <borun.p@hotmail.com>
2023-11-10 09:08:04 +0000

    and more logs

--- src/main/java/com/github/bohunn/proto/ProtobufProcessor.java
+++ src/main/java/com/github/bohunn/proto/ProtobufProcessor.java
@@ -92,6 +92,7 @@ public class ProtobufProcessor {
                 if (resultSet.next()) {
                     if (metaData.getColumnCount() > 0) {
                         LOGGER.infof("Column count: %d", metaData.getColumnCount());
+                        LOGGER.infof("Column name: %s, column type: %d", metaData.getColumnName(1), metaData.getColumnType(objTypeId));
                         QueryReturnType localQueryType = resultSet.getObject("clob", QueryReturnType.class);
                         LOGGER.infof("Returned query row: %v", localQueryType); 
                     } else {
